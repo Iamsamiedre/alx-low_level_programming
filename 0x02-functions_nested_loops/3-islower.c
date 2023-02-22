d4ab577250406ba9d364a2fa23d96d677c7b12f9
root <daresteven05@gmail.com>
2023-02-22 14:57:36 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+# include "main.h"
+/**
+ * _is lower - check for lower case letter
+ * @c : character to check the case
+ * Return: 0 or 1
+ */
+int _is lower(int c)
+
+{
+	return (c >= 97 && c <= 122);
+}
